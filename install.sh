c60f9fea897e2353565dd1e4956ec779c42b236e
RamonCollazo <73500190+RamonCollazo@users.noreply.github.com>
2026-04-19 15:38:13 -0400

    fix: install chezmoi to ~/.local/bin for PATH access

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
 
-sh -c "$(curl -fsLS get.chezmoi.io)" -- init --apply RamonCollazo
+sh -c "$(curl -fsLS get.chezmoi.io)" -- -b ~/.local/bin init --apply RamonCollazo
 cd ~ && mise install

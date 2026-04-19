439b962afe051b97950e5a73a5fd45196946c9be
RamonCollazo <73500190+RamonCollazo@users.noreply.github.com>
2026-04-19 19:13:35 -0400

    refactor: move install script to devpod/setup

--- install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-export HOME="${HOME:-/home/vscode}"
-mkdir -p "$HOME/.local/bin"
-
-curl -fsLS get.chezmoi.io -o /tmp/chezmoi-install.sh
-sh /tmp/chezmoi-install.sh -b "$HOME/.local/bin" init --apply RamonCollazo
